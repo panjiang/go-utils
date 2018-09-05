3d8909e095c8ea3563cb2ff6bce4fe4faea0014c
潘 江 <panjiang@mobartsgame.com>
2018-09-05 14:20:56 +0800

    fixbug

--- erc20/main.go
+++ erc20/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/panjiang/go/utils/currencies"
+	"github.com/panjiang/go-utils/currencies"
 	log "github.com/panjiang/golog"
 )
 
